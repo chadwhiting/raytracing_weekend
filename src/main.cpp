8d9b65592f285d89edbe98c9318edccf1ad27917
Chad Whiting <chad.e.whiting@gmail.com>
2023-08-11 21:19:01 -0700

    add a main file with a progress bar

---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main() {
+    constexpr int image_width = 256;
+    constexpr int image_height = 256;
+    constexpr float multiplier = 255.999;
+
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+    for (int j=0; j<image_height; ++j) {
+        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
+        
+        for (int i=0; i<image_width; ++i) {
+            auto r = double(i) / (image_width-1);
+            auto g = double(j) / (image_height-1);
+            auto b = 0;
+
+            int ir = static_cast<int>(multiplier * r);
+            int ig = static_cast<int>(multiplier * g);
+            int ib = static_cast<int>(multiplier * b);
+
+            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+        }
+    }
+}
+
